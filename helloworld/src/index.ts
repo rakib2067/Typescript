c0d95298efd58354ef885863d0b8901b6ad153cb
rakib2067 <rakib7@hotmail.co.uk>
2022-06-11 22:34:42 +0100

    exercises done - section 3 completed

--- helloworld/src/index.ts
+++
@@ -1,3 +0,0 @@
-function kgToLb(weight: number | string) {
-    if (typeof weight == "number") return weight * 2.2
-    else return parseInt(weight) * 2.2}
---
+++ playground/dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+let users = [{ name: 'John Smith', age: 30, occupation: 'Software engineer' }];
+console.log(users[0]);
+let shark = {
+    swim: () => console.log('swim')
+};
+let pigeon = {
+    fly: () => console.log('fly')
+};
+let puffin = {
+    swim: () => console.log('swim'),
+    fly: () => console.log('fly')
+};
+puffin.swim();
+puffin.fly();
+let day = "Saturday";
+console.log(`Today is ${day}`);
+let user = { name: 'rakib', age: 22, occupation: 'Software Engineer' };
+console.log(user === null || user === void 0 ? void 0 : user.occupation);
+//# sourceMappingURL=index.js.map
